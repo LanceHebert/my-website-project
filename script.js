a7348588fe2f24fe4430c4e3b78ae6c5e96aaf1b
Lance Hebert <LSUHEBERT@gmail.com>
2021-10-26 15:07:30 -0700

    Added technology thumbnail pic with hover

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ function clicked(element) {}
 aiPic.addEventListener("mouseover", hover);
 aiPic.addEventListener("click", clicked);
 aiPic.addEventListener("mouseout", unhover);
+
+const aiPic2 = document.getElementById("thumbnail-pic2");
+
+function hover2(element) {
+	aiPic2.setAttribute(
+		"src",
+		"https://cdn-icons-png.flaticon.com/512/4529/4529989.png"
+	);
+}
+
+function unhover2(element) {
+	aiPic2.setAttribute(
+		"src",
+		"https://cdn-icons-png.flaticon.com/512/4529/4529992.png"
+	);
+}
+
+function clicked2(element) {}
+
+aiPic2.addEventListener("mouseover", hover2);
+aiPic2.addEventListener("click", clicked2);
+aiPic2.addEventListener("mouseout", unhover2);
