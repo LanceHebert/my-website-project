6fe6f18de71c6bc6e7519da4e869d47725cc8c8c
Lance Hebert <LSUHEBERT@gmail.com>
2021-10-26 16:33:33 -0700

    Implemented onclick and movement functionality of thumbnail images

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ function hover2(element) {
 		"src",
 		"https://cdn-icons-png.flaticon.com/512/4529/4529989.png"
 	);
-	eduList.setAttribute("zIndex", "-1");
 }
 
 function unhover2(element) {
@@ -43,7 +42,30 @@ function unhover2(element) {
 	);
 }
 
-function clicked2(element) {}
+const brainPic = document.querySelector("#thumbnail-pic2");
+const eduLi1 = document.querySelector(
+	"#container-info > div:nth-child(2) > ul > li:nth-child(1)"
+);
+const eduLi2 = document.querySelector(
+	"#container-info > div:nth-child(2) > ul > li:nth-child(2)"
+);
+const eduLi3 = document.querySelector(
+	"#container-info > div:nth-child(2) > ul > li:nth-child(3)"
+);
+
+function clicked2(element) {
+	if (brainPic.style.left === "90%") {
+		brainPic.style.left = "10%";
+		eduLi1.style.zIndex = "100";
+		eduLi2.style.zIndex = "100";
+		eduLi3.style.zIndex = "100";
+	} else {
+		brainPic.style.left = "90%";
+		eduLi1.style.zIndex = "-1";
+		eduLi2.style.zIndex = "-1";
+		eduLi3.style.zIndex = "-1";
+	}
+}
 
 aiPic2.addEventListener("mouseover", hover2);
 aiPic2.addEventListener("click", clicked2);
