06d03d3753b32fb65acef38c947224b030a55390
Lance Hebert <LSUHEBERT@gmail.com>
2021-10-25 19:36:38 -0700

    Added education list and technology list

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 // h2.textContent = "This content added by JavaScript";
 
 // document.querySelector("body").appendChild(h2);
+
+const aiPic = document.getElementById("thumbnail-pic");
+
+function hover(element) {
+	aiPic.setAttribute(
+		"src",
+		"https://cdn-icons-png.flaticon.com/512/4529/4529976.png"
+	);
+}
+
+function unhover(element) {
+	aiPic.setAttribute(
+		"src",
+		"https://cdn-icons-png.flaticon.com/512/4529/4529980.png"
+	);
+}
+
+function clicked(element) {}
+
+aiPic.addEventListener("mouseover", hover);
+aiPic.addEventListener("click", clicked);
+aiPic.addEventListener("mouseout", unhover);
